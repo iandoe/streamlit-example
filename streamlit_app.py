fee8885648c4f9c9203f64011ba7c2e537537858
Guillaume Picard <wilhearts@gmail.com>
2024-02-12 14:34:24 +0700

    Init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,65 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import instructor
+from openai import OpenAI
+from pydantic import BaseModel
+import datetime
+import PyPDF2
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Extract data from a PDF invoice using ChatGPT
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="openai_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+client = instructor.patch(
+    OpenAI(
+        api_key=openai_api_key,
+    ),
+)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+invoice_file = st.file_uploader("Upload a PDF invoice", type=["pdf"])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+class Invoice(BaseModel) :
+    date: datetime.date
+    reference: str
+    supplier: str
+    currency: str
+    total_amount: float
+    total_tax_amount: float
+    description: str
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def extract_text_from_pdf(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ''
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        text += page.extract_text()
+    return text
+
+if invoice_file:
+    pdf_text = extract_text_from_pdf(invoice_file)
+    invoice = client.chat.completions.create(
+        temperature=0,
+        response_model=Invoice,
+        messages=[
+            {
+                "role": "user",
+                "content": f"""
+                    Extract, 
+                    while making sure currency is in ISO 4217 format,
+                    infer the reference from the invoice number,
+                    and the date in ISO 8601 format:
+                    
+                    {pdf_text}
+                """,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    st.json(invoice.model_dump_json(), expanded=True)
+
+
+
